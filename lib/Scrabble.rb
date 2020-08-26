f60f5bd37284c6334509454af5a26fff1dfb1a55
Joshua Lovelace <rakmial@gmail.com>
2020-08-26 11:06:26 -0700

    test 3 green

--- lib/Scrabble.rb
+++ lib/Scrabble.rb
@@ -1,22 +1,22 @@
 require('pry')
 
 class Scrabble
-  #scoring = {
-  #  ['A','E','I','O','U','L','N','R','S','T'] => 1,
-  #  ['D','G'] => 2,
-  #  ['B','C','M','P'] => 3,
-  #  ['F','H','V','W','Y'] => 4,
-  #  'K' => 5,
-  #  ['J','X'] => 8,
-  #  ['Q','Z'] => 10
-  #  }
   def initialize(word)
+    @scoring = {
+      ['A','E','I','O','U','L','N','R','S','T'] => 1,
+      ['D','G'] => 2,
+      ['B','C','M','P'] => 3,
+      ['F','H','V','W','Y'] => 4,
+      'K' => 5,
+      ['J','X'] => 8,
+      ['Q','Z'] => 10
+      }
     @word = word
   end
   def word
     @word
   end
-  #def getscoring
-  #  scoring
-  #end
+  def getscoring
+    @scoring
+  end
 end
--- spec/Scrabble_spec.rb
+++ spec/Scrabble_spec.rb
@@ -27,6 +27,6 @@ end
   ### Sequence of tests to pass from simple to complex
   ### must be able to create new Scrabble objects from class Scrabble
   ### Scrabble initializes with var word bound to @word, reader method to check
-  # class Scrabble has class var Hash named scoring of key arrays of letters to
-  #    score values, getscore() method to check
+  ### class Scrabble has class var Hash named scoring of key arrays of letters to
+  ###    score values, getscoring() method to check
   # method score returns value of entire word
