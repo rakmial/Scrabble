8730d39e8ad2ecd6177eb57632b788ccc845a4a1
Joshua Lovelace <rakmial@gmail.com>
2020-08-26 10:17:57 -0700

    plain English spec written

---
+++ spec/Scrabble_spec.rb
@@ -0,0 +1,8 @@
+require('rspec')
+require('Scrabble')
+
+# Sequence of tests to pass from simple to complex
+# must be able to create new Scrabble objects from class Scrabble
+# Scrabble initializes with var word bound to @word, reader method to check
+# class Scrabble has class var Hash named scoring of key arrays of letters to score values, getscore() method to check
+# method score returns value of entire word
