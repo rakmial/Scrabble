463d0315255f43ecc5f0b1510ac379c1a352a5a9
Joshua Lovelace <rakmial@gmail.com>
2020-08-26 10:25:07 -0700

    test 1 red

--- lib/Scrabble.rb
+++ lib/Scrabble.rb
@@ -1,2 +1,4 @@
 require('pry')
 
+#class Scrabble
+#end
--- spec/Scrabble_spec.rb
+++ spec/Scrabble_spec.rb
@@ -1,7 +1,14 @@
 require('rspec')
 require('Scrabble')
 
-# Sequence of tests to pass from simple to complex
+describe('Scrabble') do
+  it('must create new Scrabble objects from class Scrabble') do
+    expect(Scrabble.new.class).to(eq(Scrabble))
+  end
+end
+
+
+### Sequence of tests to pass from simple to complex
 # must be able to create new Scrabble objects from class Scrabble
 # Scrabble initializes with var word bound to @word, reader method to check
 # class Scrabble has class var Hash named scoring of key arrays of letters to score values, getscore() method to check
